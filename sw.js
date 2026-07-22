357060a209c94bee4b73cb432f69ccd39238f3a7
sandip01004-cell <sandip01004@gmail.com>
2026-07-23 02:14:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "focusflow-v1";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./script.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
